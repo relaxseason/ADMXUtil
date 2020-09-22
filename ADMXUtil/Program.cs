68b416e2e48653b7bc48a4d575f3ce686e77580d
relaxseason <relax.season@gmail.com>
2020-09-22 13:08:06 +0900

    プロジェクト ファイルを追加します。

---
+++ ADMXUtil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADMXUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
